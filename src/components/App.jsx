e7e75f31605c88176f6dc696f1798fc9599c434a
Laura Neves <lauracmneves@gmail.com>
2022-05-20 15:43:18 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+function App(){
+    return(
+        <p>hello word</p>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react-dom";
+import App from "./components/App";
+import "./assets/reset.css"
+import "./assets/styles.css"
+
+
+
+React.render(<App />, document.querySelector(".root"))
